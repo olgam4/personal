f8f99bedece9df1895001fefdc795c6634739d83
Olivier Gamache <olivier.gamache@elapsetech.com>
2021-07-19 16:06:48 -0400

    Better design and flourishes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import './App.css'
-
 import Link from './components/Link'
+
+import cat from './images/cat.png'
+
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-        <div>
+        <div className="App-content">
             <h1>
                 Olivier Gamache
             </h1>
@@ -15,9 +17,12 @@ function App() {
                 <li><Link name="linkedin" url="https://linkedin.com/in/olivier-gamache-1337420" /></li>
                 <li><Link name="medium" url="https://oliviergamache.medium.com" /></li>
             </ul>
+            <div className="App-catchphrase">
+                graphic design is my passion
+            </div>
         </div>
-        <div className="App-catchphrase">
-            graphic design is my passion
+        <div>
+            <img src={cat} alt="awesome cat" width="200px" />
         </div>
     </div>
   )
